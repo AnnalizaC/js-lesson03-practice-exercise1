57d8472ce521b01bd6d0795931d82f194de30f13
Annaliza_C <aldcadiente@gmail.com>
2024-02-05 19:20:51 -0800

    Initial commit.

---
+++ script.js
@@ -0,0 +1,10 @@
+var twelveString = "12";
+var twelveNum = 12;
+
+if (twelveString === twelveNum) {
+  console.log("The value and type of these two variables are the same.");
+} else if (twelveString == twelveNum) {
+  console.log("The value of these two variables are the same.");
+} else {
+  console.log("The value and type of these two variables are different.");
+}
